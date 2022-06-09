c044303121c3843b6ae63991701e6076f06a5978
dewey preast <webwalkin@me.com>
2022-06-09 19:11:28 -0400

    working to resolve conflicts

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require("../models/user");
+const Customer = require("../models/customer");
+
+User.hasMany(Customer, {
+  foreignKey: "customer_id",
+  onDelete: "cascade",
+});
+
+User.belongsTo(User, {
+  primaryKey: "id",
+});
+
+module.exports = { User, Customer };
