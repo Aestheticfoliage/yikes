95113b2a837be267ec1a37b1351abcf4f2ce1918
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-06-18 14:32:31 -0400

    worked on fixing bugs. changed some routes, models, seeds.

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -29,7 +29,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!Review) {
-      res.status(404).json({ message: 'No review found with this id!' });
+      res.status(404).json({ message: 'No reviews found with this id!' });
       return;
     }
 
