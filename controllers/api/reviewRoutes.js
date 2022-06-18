678fa619893b8b86689aba11b4b056fc147adcc5
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-06-18 14:26:59 -0400

    changed some routes.

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Review } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newReview = await Review.create({
+      ...req.body,
+      userId: req.session.user_id,
+    });
+
+    console.log('newPost', newReview);
+
+    res.status(200).json(newReview);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const reviewData = await Review.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!Review) {
+      res.status(404).json({ message: 'No reviews found with this id!' });
+      return;
+    }
+
+    res.status(200).json(reviewData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
